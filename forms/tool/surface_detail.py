9bb626f5ef1a7a0d4b11da8db5dde073d5e9d99c
David Paul Rosser <info@davidpaulrosser.co.uk>
2013-08-17 14:59:59 +0100

    Initial commit of surface tool

---
+++ forms/tool/surface_detail.py
@@ -0,0 +1,140 @@
+import pymel.core as pm
+import pymel.core.datatypes as dt
+import pymel.util as util
+import random
+from functools import partial
+
+class SurfaceDetail:
+
+	def __init__( self ):
+		self.reset()
+
+	def reset( self ):
+
+		self.iterations       = 3
+		self.radius           = 5
+		self.labels           = [ ]
+		self.sliders          = [ ]
+		self.sliderValues     = [ None ] * self.iterations
+		self.meshTypes 		  = { 'Soccer Ball': 0, 'Dodecahedron': 0, 'Icosahedron': 1, 'Octahedron': 2, 'Tetrahedron': 3 } 
+		self.meshType         = 'Soccer Ball'
+		self.operations       = [ 'poke', 'triangle', 'quad' ]
+		self.operation        = 'poke'
+		self.mesh         	  = None
+		self.indicies     	  = [ ]
+		self.vertexIndicies   = [ ]
+		self.vertexPositions  = [ ]
+
+
+	def generate( self, meshType, radius, iterations, operation ):
+		
+		self.reset()
+
+		self.meshType   = meshType
+		self.radius     = radius
+		self.iterations = iterations
+		self.operation  = operation
+
+		if self.meshType == 'Soccer Ball':
+			self.mesh = pm.polyPrimitive( radius = self.radius, polyType = self.meshTypes[ self.meshType ] )
+		else:
+			self.mesh = pm.polyPlatonicSolid( radius = self.radius, solidType = self.meshTypes[ self.meshType ] )
+
+		self.vertexIndicies = self.__subdivide()
+
+		# Store each vertex position from each iteration
+		for vertex in self.mesh[0].vtx:
+			position = vertex.getPosition()
+			self.vertexPositions.append( position )
+
+		return self.mesh
+
+
+	def setScale( self, scaleValues ):
+
+		length = len( self.vertexIndicies )
+
+		verticesStartIndex = 0
+		verticesEndIndex   = 1
+
+		for i in range( self.iterations ):
+
+			if i is 0:
+				verticesStartIndex = 0
+				verticesEndIndex   = self.vertexIndicies[ 0 ]
+			else:
+				verticesStartIndex = self.vertexIndicies[ i - 1 ]
+				verticesEndIndex   = self.vertexIndicies[ i ]
+
+			for j in range( verticesStartIndex, verticesEndIndex ):
+				position = self.vertexPositions[ j ] * scaleValues[ i ]
+				self.mesh[ 0 ].vtx[ j ].setPosition( position )
+
+
+	def __subdivide( self ):
+
+		vertices = [ ]
+
+		for i in range( self.iterations ):
+			
+			_faces    = self.mesh[ 0 ].f
+			_vertices = self.mesh[ 0 ].vtx
+			length   = len( _vertices )
+
+			if self.operation == 'quad': divisionMode = 0
+			if self.operation == 'triangle': divisionMode = 1
+			
+			if self.operation == 'poke':
+				pm.polyPoke( _faces, translateX = 0, translateY = 0, translateZ = 0, worldSpace = True )
+			else:
+				pm.polySubdivideFacet( _faces, mode = divisionMode, divisions = 1 )
+
+			vertices.append( length )
+
+		pm.select( clear = True )
+
+		return vertices
+
+
+
+# Generate some random solids
+
+tool = SurfaceDetail()
+
+# for key in tool.meshTypes:
+
+def surfaceDetailSolidGenerator():
+
+	rows = 1
+	cols = 1
+
+	radius      = 10
+	iterations  = 4
+	scaleValues = []
+	minScale    = 0.1
+	maxScale    = 0.15
+
+	for col in range(cols):
+		for row in range(rows):
+
+			x = col * (radius * 4)
+			z = row * (radius * 4)
+
+			for i in range(iterations):
+
+				if i is 0:
+					newScale = 1
+				else:
+					newScale = scaleValues[i-1] + random.uniform(minScale, maxScale) 
+
+				scaleValues.append( newScale )
+
+			meshType  = random.choice(tool.meshTypes.keys())
+			operation = random.choice(tool.operations)
+
+			mesh = tool.generate(meshType, radius, iterations, 'poke')
+			tool.setScale(scaleValues)
+			mesh[0].setTranslation([x, 0, z])
+
+
+surfaceDetailSolidGenerator()
